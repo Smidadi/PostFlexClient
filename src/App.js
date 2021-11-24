f1d63526c10b8b20087cab602fc5e801b58d0f3c
Labayanne <yannblanchet@orange.fr>
2021-11-24 15:09:43 +0100

    Initial front structure

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import KanbanColumn from './components/KanbanColumn';
+import Authentification from './components/Authentification';
+import MainInfosBar from './components/MainInfosBar';
+import Sprint from './components/Sprint';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: ''};
+  }
+
+  callAPI() {
+      fetch("http://localhost:9000/testAPI")
+          .then(res => res.text())
+          .then(res => this.setState({ apiResponse: res }))
+          .catch(err => err);
+  }
+
+  componentDidMount() {
+      this.callAPI();
+  }
+
+  render() {
+    return (
+<div>
+<header>
+<div class="container-fluid">
+    <div class="row">
+      <MainInfosBar />
+      <Authentification />
+    </div>
+</div>
+</header>
+<div class="container-fluid">
+<div class="row border border-dark">
+    <KanbanColumn />
+    <div class="col-8">
+        <div class="row">
+           <Sprint />
+        </div>
+    </div>
+</div>
+</div>
+</div>
+    )
+  }
+}
+
+export default App;
