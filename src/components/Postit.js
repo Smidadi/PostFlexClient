41c41f684d7d9b833846ea7a33bc138703b4ca00
Labayanne <yannblanchet@orange.fr>
2021-12-01 14:15:11 +0100

    Added current date in postits

--- src/components/Postit.js
+++ src/components/Postit.js
@@ -4,12 +4,25 @@ class Postit extends Component {
 
     constructor(props) {
         super(props);
+        var today = new Date(),
+            date = today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear();
+        this.state = {date: date};
     }
+
+
 
     render() {
         return (
             <div>
-                {this.props.title}
+                <div>
+                    {this.props.title}
+                </div>
+                <div>
+                    {this.props.description}
+                </div>
+                <div>
+                    {this.state.date}
+                </div>  
             </div>
         )
     }
