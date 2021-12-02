178c8af347861bb35d71acaff8d5e70ba57cd18d
Labayanne <yannblanchet@orange.fr>
2021-12-02 11:01:50 +0100

    Improvement of the productBacklog visual

--- src/components/Postit.js
+++ src/components/Postit.js
@@ -12,23 +12,23 @@ class Postit extends Component {
 
     render() {
         return (
-            <div>
-                {/* List of rectangles of chosen colors */}
-                {this.props.colors != null ? this.props.colors.map((color, index) => 
-                <div key={index} style={{ display: "flex",  width: "50x", 
-                    height: "10px", backgroundColor: color}}>
-                </div>
-                ) : null}
-                
-                <div>
-                    {this.props.title != '' ? this.props.title : "Titre"}
-                </div>
-                <div>
-                    {this.props.description != '' ? this.props.description : "Description"}
-                </div>
-                <div>
-                    {this.state.date}
-                </div>  
+            <div class="col-12 column">
+                    {/* List of rectangles of chosen colors */}
+                    {this.props.colors != null ? this.props.colors.map((color, index) => 
+                    <div key={index} style={{ display: "flex",  width: "50x", 
+                        height: "10px", backgroundColor: color}}>
+                    </div>
+                    ) : null}
+                    
+                    <h6>
+                        {this.props.title != '' ? this.props.title : "Titre"}
+                    </h6>
+                    <div>
+                        {this.props.description != '' ? this.props.description : "Description"}
+                    </div>
+                    <div>
+                        {this.state.date}
+                    </div>  
             </div>
         )
     }
